d91b2b851f8980e8e170cbe888bb29698a2f33cc
SHERSHAM <shershamj@gmail.com>
2026-02-27 21:07:04 +0530

    usecase2

---
+++ UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        System.out.println("Hardcoded String: " + word);
+
+        if (word.equals(reversed)) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+    }
+}
